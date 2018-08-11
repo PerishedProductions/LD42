d19412d3e5da2630a1ffa6bfee1b3bf4d133a537
Mikkel Mouridsen <mikkelmouridsen@me.com>
2018-08-11 12:46:08 +0200

    Attack hitboxes added!

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -5,8 +5,16 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
     public int movementSpeed = 500;
+    public float attackTime = 1;
+
+    public Collider2D hitbox;
+    public Vector2[] hitboxPos;
 
     private Rigidbody2D rb;
+
+    private bool attack = false;
+    private bool canAttack = true;
+    private float currAttackTime = 0;
 
 	// Use this for initialization
 	void Start () {
@@ -14,13 +22,68 @@ public class PlayerController : MonoBehaviour {
         rb = GetComponent<Rigidbody2D>();
 
 	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
+
+    private void Update()
+    {
+        Vector2 movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        if (Input.GetButtonDown("Fire1") && canAttack)
+        {
+            attack = true;
+            canAttack = false;
+        }
+
+        if (attack)
+        {
+
+            hitbox.enabled = true;
+
+            currAttackTime += Time.deltaTime;
+
+            if (currAttackTime >= attackTime)
+            {
+                canAttack = true;
+                attack = false;
+                currAttackTime = 0;
+            }
+
+            if (movement.x < 0)
+            {
+                hitbox.offset = hitboxPos[0];
+            }
+            else if (movement.x > 0)
+            {
+                hitbox.offset = hitboxPos[1];
+            }
+
+            if (movement.y < 0)
+            {
+                hitbox.offset = hitboxPos[3];
+            }
+            else if (movement.y > 0)
+            {
+                hitbox.offset = hitboxPos[2];
+            }
+        }
+        else
+        {
+            hitbox.enabled = false;
+        }
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate () {
 
 		Vector2 movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
 
         rb.velocity = movement * movementSpeed * Time.deltaTime;
 
-	}
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        canAttack = true;
+        attack = false;
+    }
 }
