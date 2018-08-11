1fb155f359cee825419695cc1016e96c3ac05cef
Mikkel Mouridsen <mikkelmouridsen@me.com>
2018-08-11 10:44:59 +0200

    Depthhhhhhhh

---
+++ Assets/Scripts/World/Depth.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Depth : MonoBehaviour {
+
+
+    public bool update = false;
+
+    private void Start()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.y);
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        if (update)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.y);
+        }
+
+	}
+}
