15bd53735b1c5b42050975ac807c018b2b30f676
Mikkel Mouridsen <mikkelmouridsen@me.com>
2018-08-11 21:39:46 +0200

    Wonky Soul running away

--- Assets/Scripts/NPC/Enemies/Enemy.cs
+++ Assets/Scripts/NPC/Enemies/Enemy.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,6 +8,7 @@ public class Enemy : MonoBehaviour {
     public enum EnemyState { Idle, Defensive, Aggressive }
 
     public EnemyState state = EnemyState.Idle;
+    public int moveSpeed = 1;
 
     private Rigidbody2D rb;
 
@@ -15,7 +17,7 @@ public class Enemy : MonoBehaviour {
 
         rb = GetComponent<Rigidbody2D>();
 
-        int rand = Random.Range(0, 2);
+        int rand = UnityEngine.Random.Range(0, 2);
 
         switch (rand)
         {
@@ -44,15 +46,38 @@ public class Enemy : MonoBehaviour {
 
                 var targets = Physics2D.OverlapCircleAll(rb.position, 5);
 
+                GameObject target = null;
+
                 if (targets != null)
                 {
+
+                    bool playerFound = false;
+
                     for (int i = 0; i < targets.Length; i++)
                     {
                         if (targets[i].tag == "Player")
                         {
-                            rb.velocity = targets[i].GetComponent<Rigidbody2D>().velocity / 2;
+                            target = targets[i].gameObject;
+                            playerFound = true;
                         }
+                        
                     }
+
+                    if (target != null && playerFound == false)
+                    {
+                        target = null;
+                    }
+
+                }
+
+                if (target != null)
+                {
+                    var direction = target.transform.position - transform.position;
+                    rb.velocity = -Vector2.Lerp(Vector2.zero, direction, 0.5f) * moveSpeed * Time.deltaTime;
+                }
+                else
+                {
+                    rb.velocity = Vector2.zero;
                 }
 
                 break;
@@ -61,4 +86,5 @@ public class Enemy : MonoBehaviour {
         }
 
     }
+
 }
