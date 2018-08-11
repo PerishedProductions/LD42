6eb26e0f58f304cb21a9baff0cb34602ee4ebde3
Dragion <Dragion147@hotmail.com>
2018-08-11 18:48:08 +0200

    Add weapon stuff

---
+++ Assets/Scripts/Weapons/Weapon.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Weapon : MonoBehaviour {
+
+    public float Reloadtime = 1f;
+    public int MagazineSize = 1;
+    public float Range = 5;
+    protected int _ammoLeft = 0;
+    protected float _currentReloadtime;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
