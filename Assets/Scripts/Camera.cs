c5f2197eee7a88d35878c138d81efd0b35a9c86b
Mikkel Mouridsen <mikkelmouridsen@me.com>
2018-08-11 13:03:02 +0200

    Rendering fixes + Civilian prefab

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour {
+
+    public Transform target;
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
+
+        
+
+	}
+}
