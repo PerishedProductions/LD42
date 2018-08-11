d2761a2526fcfe19b5e972c11d626ab3732b339a
Mikkel Mouridsen <mikkelmouridsen@me.com>
2018-08-11 10:20:48 +0200

    Player movement!

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public int movementSpeed = 500;
+
+    private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+
+        rb = GetComponent<Rigidbody2D>();
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		Vector2 movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        rb.velocity = movement * movementSpeed * Time.deltaTime;
+
+	}
+}
