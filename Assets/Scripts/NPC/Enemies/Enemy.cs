54e313970b4b58f7d5a3c99ba7f125b906cb4440
Mikkel Mouridsen <mikkelmouridsen@me.com>
2018-08-11 20:01:19 +0200

    Enemy Stuffs

--- Assets/Scripts/NPC/Enemies/Enemy.cs
+++ Assets/Scripts/NPC/Enemies/Enemy.cs
@@ -4,13 +4,61 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour {
 
+    public enum EnemyState { Idle, Defensive, Aggressive }
+
+    public EnemyState state = EnemyState.Idle;
+
+    private Rigidbody2D rb;
+
 	// Use this for initialization
 	void Start () {
-		
-	}
+
+        rb = GetComponent<Rigidbody2D>();
+
+        int rand = Random.Range(0, 2);
+
+        switch (rand)
+        {
+            case 0:
+                state = EnemyState.Idle;
+                break;
+            case 1:
+                state = EnemyState.Defensive;
+                break;
+            case 2:
+                state = EnemyState.Aggressive;
+                break;
+        }
+
+    }
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void Update ()
+    {
+
+        switch (state)
+        {
+            case EnemyState.Idle:
+                break;
+            case EnemyState.Defensive:
+
+                var targets = Physics2D.OverlapCircleAll(rb.position, 5);
+
+                if (targets != null)
+                {
+                    for (int i = 0; i < targets.Length; i++)
+                    {
+                        if (targets[i].tag == "Player")
+                        {
+                            rb.velocity = targets[i].GetComponent<Rigidbody2D>().velocity / 2;
+                        }
+                    }
+                }
+
+                break;
+            case EnemyState.Aggressive:
+                break;
+        }
+
+    }
 }
